132dc2813511d7dcb72af63d703e0757eba1357b
Andrew Weymes <andrew.weymes@outlook.com>
2024-07-13 12:27:07 +0800

    add http server

--- Backend/cmd/main.go
+++ Backend/cmd/main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	"github.com/labstack/echo/v4"
+)
+
+func init() {
+	// load environment variables
+	if err := godotenv.Load(".env"); err != nil {
+		log.Panicf("Failed to load environment variables: %v", err)
+	}
+}
 
 func main() {
-	fmt.Println("Hello, World!")
+	e := echo.New()
+
+	e.Logger.Fatal(e.Start(":" + os.Getenv("PORT")))
 }
