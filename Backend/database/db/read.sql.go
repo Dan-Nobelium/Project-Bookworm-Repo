0de3fb5d4172ffb7ebfaa064d518b3822be6fa03
Andrew Weymes <andrew.weymes@outlook.com>
2024-07-13 14:57:22 +0800

    add sql and go code to perform an admin login

--- Backend/database/db/read.sql.go
+++ Backend/database/db/read.sql.go
@@ -7,6 +7,7 @@ package db
 
 import (
 	"context"
+	"database/sql"
 )
 
 const getAdminByEmail = `-- name: GetAdminByEmail :one
@@ -29,6 +30,49 @@ func (q *Queries) GetAdminByEmail(ctx context.Context, email string) (Admin, err
 		&i.Superuser,
 		&i.CreatedAt,
 		&i.UpdatedAt,
+	)
+	return i, err
+}
+
+const getAdminLogin = `-- name: GetAdminLogin :one
+SELECT
+	id, first_name, last_name, email, superuser, a.created_at, a.updated_at, admin_id, password_hash, c.created_at, c.updated_at
+FROM
+	admin AS a
+	LEFT JOIN credential AS c ON a.id = c.admin_id
+WHERE
+	a.id = ?
+`
+
+type GetAdminLoginRow struct {
+	ID           string
+	FirstName    string
+	LastName     string
+	Email        string
+	Superuser    int64
+	CreatedAt    int64
+	UpdatedAt    int64
+	AdminID      sql.NullString
+	PasswordHash []byte
+	CreatedAt_2  sql.NullInt64
+	UpdatedAt_2  sql.NullInt64
+}
+
+func (q *Queries) GetAdminLogin(ctx context.Context, id string) (GetAdminLoginRow, error) {
+	row := q.db.QueryRowContext(ctx, getAdminLogin, id)
+	var i GetAdminLoginRow
+	err := row.Scan(
+		&i.ID,
+		&i.FirstName,
+		&i.LastName,
+		&i.Email,
+		&i.Superuser,
+		&i.CreatedAt,
+		&i.UpdatedAt,
+		&i.AdminID,
+		&i.PasswordHash,
+		&i.CreatedAt_2,
+		&i.UpdatedAt_2,
 	)
 	return i, err
 }
--- Backend/database/sql/queries/auth/read.sql
+++ Backend/database/sql/queries/auth/read.sql
@@ -6,6 +6,15 @@ FROM
 WHERE
 	email = ?;
 
+-- name: GetAdminLogin :one
+SELECT
+	*
+FROM
+	admin AS a
+	LEFT JOIN credential AS c ON a.id = c.admin_id
+WHERE
+	a.id = ?;
+
 -- name: GetSession :one
 SELECT
 	*
