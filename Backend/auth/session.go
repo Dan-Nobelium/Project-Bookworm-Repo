824bed3a4d07b71fcef855c0aaf439290803275e
Andrew Weymes <andrew.weymes@outlook.com>
2024-07-13 19:17:23 +0800

    add function to generate session cookie and store in database

---
+++ Backend/auth/session.go
@@ -0,0 +1,48 @@
+package auth
+
+import (
+	"crypto/rand"
+	"encoding/base64"
+	"net/http"
+	"project-bookworm/database"
+	"project-bookworm/database/db"
+	"time"
+
+	"github.com/labstack/echo/v4"
+)
+
+// creates and attaches a session cookie and stores session in database
+func CreateSession(ctx echo.Context, admin *db.GetAdminLoginRow) error {
+	// generate a random session ID
+	bytes := make([]byte, 32)
+	if _, err := rand.Read(bytes); err != nil {
+		return err
+	}
+
+	session := base64.URLEncoding.EncodeToString(bytes)
+
+	// expire after 15 minutes
+	expiry := time.Now().Add(time.Minute * 15)
+
+	// set session cookie
+	ctx.SetCookie(&http.Cookie{
+		Name:     "session",
+		Value:    session,
+		Expires:  expiry,
+		HttpOnly: true,
+		Secure:   true,
+		SameSite: http.SameSiteStrictMode,
+	})
+
+	// add session to database
+	if err := database.DB.Queries.CreateSession(ctx.Request().Context(), db.CreateSessionParams{
+		ID:      session,
+		AdminID: admin.ID,
+		Expiry:  expiry.Unix(),
+	}); err != nil {
+		return err
+	}
+
+	// return nil error
+	return nil
+}
