b08c09ee4dd3f5c8b483d58eb87fb0732e08b2c8
Andrew Weymes <andrew.weymes@outlook.com>
2024-07-16 11:24:50 +0800

    add handler function for admin home page and update route with middleware

--- Backend/handlers/routes.go
+++ Backend/handlers/routes.go
@@ -1,17 +1,11 @@
 package handlers
 
 import (
-	"net/http"
-	"project-bookworm/templates"
-	"project-bookworm/templates/pages"
+	"project-bookworm/auth"
 
 	"github.com/labstack/echo/v4"
 )
 
-func TestHandler(ctx echo.Context) error {
-	return templates.Render(ctx, http.StatusOK, pages.TestPage("Hello, World!"))
-}
-
 func InitHandlerRoutes(e *echo.Echo) {
-	e.GET("/", TestHandler)
+	e.GET("/", AdminHome, auth.Admin)
 }
