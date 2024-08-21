774661f491b8df1efd5c2fb51cc1d64ed6ef3bbf
Matt Ralston <mralston@myune.edu.au>
2024-08-21 20:48:53 +1000

    revert timeline.push line commenting

--- Frontend/app2.js
+++ Frontend/app2.js
@@ -1222,9 +1222,9 @@ let timeline = []; // This is the master timeline, the experiment runs sequentia
 // timeline.push(instructionCheckWithFeedback);
 
 // // // Attention check
-// timeline.push(cfi_block);
-// timeline.push(htq_block);
-// timeline.push(audit_block);
+timeline.push(cfi_block);
+timeline.push(htq_block);
+timeline.push(audit_block);
 
 // // Phase 1, no ships
 // addBlocksToTimeline(timeline, planet_noship, nBlocks_p1, nTrialspBlk);
@@ -1238,7 +1238,7 @@ let timeline = []; // This is the master timeline, the experiment runs sequentia
 
 // // Phase2, ships
 // timeline.push(phaseTwoInstructions);
-addBlocksToTimeline(timeline, planet_ship, nBlocks_p2, nTrialspBlk);
+// addBlocksToTimeline(timeline, planet_ship, nBlocks_p2, nTrialspBlk);
 // timeline.push(valence_p2);
 // timeline.push(infer_p2_A);
 // timeline.push(infer_p2_B);
@@ -1250,7 +1250,7 @@ addBlocksToTimeline(timeline, planet_ship, nBlocks_p2, nTrialspBlk);
 // timeline.push(p1_q4_triangle);
 
 // Phase3, contingencies
-// timeline.push(cont_catch);
+timeline.push(cont_catch);
 
 // Phase3, ships
 // addBlocksToTimeline(timeline, planet_ship, nBlocks_p3, nTrialspBlk);
