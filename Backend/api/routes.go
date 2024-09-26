21e41049a87b88b4d046917fd209ca6af38d40ff
Dan-Nobelium <Daniel.nobelium@gmail.com>
2024-09-26 17:08:28 +1000

    revmoed backend and .gitattributes

--- Backend/api/routes.go
+++
@@ -1,8 +0,0 @@
-package api
-
-import "github.com/labstack/echo/v4"
-
-func InitApiRoutes(e *echo.Echo) {
-	e.POST("/login", Login)
-	e.GET("/logout", Logout)
-}
--- Backend/database/sql/schema/003_session.sql
+++
@@ -1,9 +0,0 @@
--- +goose Up
-CREATE TABLE IF NOT EXISTS session (
-  id TEXT PRIMARY KEY,
-  admin_id TEXT NOT NULL REFERENCES admin(id) ON DELETE CASCADE,
-  expiry INT NOT NULL
-) STRICT;
-
--- +goose Down
-DROP TABLE session;
